ae41e199c9bc46c8478c740128d401a677b372b3
MaxZ <maxzonoff@gmail.com>
2026-03-12 01:06:38 +0300

    dz_24

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,19 +4,54 @@ from .models import *
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "description", "image", "category", "price", "created_at", "updated_at",)
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "image",
+        "category",
+        "price",
+        "created_at",
+        "updated_at",
+    )
     list_filter = ("category",)
-    search_fields = ("name", "description",)
+    search_fields = (
+        "name",
+        "description",
+    )
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "description", "created_at", "updated_at",)
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
     list_filter = ("name",)
-    search_fields = ("name", "description",)
+    search_fields = (
+        "name",
+        "description",
+    )
 
 
 @admin.register(Contacts)
 class ContactsAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "phone", "message", "created_at", "updated_at",)
-    list_filter = ("name", "phone",)
-    search_fields = ("name", "message",)
+    list_display = (
+        "id",
+        "name",
+        "phone",
+        "message",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "name",
+        "phone",
+    )
+    search_fields = (
+        "name",
+        "message",
+    )
--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -3,30 +3,38 @@ from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
-    help("Add products in database")
+    help("Добавление продуктов в базу данных")
 
     def handle(self, *args, **options):
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-        category, _ = Category.objects.get_or_create(name="Категория_тест", description="Описание_тест")
+        category, _ = Category.objects.get_or_create(
+            name="Категория_тест", description="Описание_тест"
+        )
 
         products = [
-            {"name": "Товар_test",
-             "description": "Описание",
-             "category": category,
-             "price": "123.00"
-             },
-            {"name": "Товар_7",
-             "description": "Описание",
-             "category": category,
-             "price": "321.00"
-             },
+            {
+                "name": "Товар_test",
+                "description": "Описание",
+                "category": category,
+                "price": "123.00",
+            },
+            {
+                "name": "Товар_7",
+                "description": "Описание",
+                "category": category,
+                "price": "321.00",
+            },
         ]
 
         for prod_data in products:
             prod, created = Product.objects.get_or_create(**prod_data)
             if created:
-                self.stdout.write(self.style.SUCCESS(f"Successfully added product: {prod.name}"))
+                self.stdout.write(
+                    self.style.SUCCESS(f"Successfully added product: {prod.name}")
+                )
             else:
-                self.stdout.write(self.style.WARNING(f"Book already exist: {prod.name}"))
+                self.stdout.write(
+                    self.style.WARNING(f"Book already exist: {prod.name}")
+                )
